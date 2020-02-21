33076deae1e0778b22df7cf4f629446d191302bf
Alex <robert.perez6@g.austincc.edu>
2020-02-21 13:49:30 -0600

    added readme

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://agile-bayou-84720.herokuapp.com/api',
     TOKEN_KEY: 'secret',
   }
   //API_ENDPOINT: 'https://agile-bayou-84720.herokuapp.com/api',
+  //API_ENDPOINT: 'http://localhost:8000/api'
